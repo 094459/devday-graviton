acd8f3589921d65f0d8f0e5ad069558d55e35584
ricsue <ricsue@amazon.com>
2021-03-26 13:59:57 +0000

    fix zip file

--- mulit-arch/multiarch-ami/codecommit-repo/src/main/java/hello/HelloController.java
+++ mulit-arch/multiarch-ami/codecommit-repo/src/main/java/hello/HelloController.java
@@ -10,7 +10,10 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello World from Immutable EC2 Instance\n";
+        String str = "<html><body><h1>EKS Spring Boot application</h1><br><p>Java Runtime version is " + System.getProperty("java.runtime.version") + " from "+ System.getProperty("java.vm.name") + " and my Java home is " + System.getProperty("java.home") + "</p><br>\n";
+        str = str.concat("<p1>Operating System details: ").concat(System.getProperty("os.arch")).concat(" ").concat(System.getProperty("os.name")).concat(" ").concat(System.getProperty("os.version")).concat("</p><br>\n");
+        str = str.concat("Hope you enjoy the talk!<br><h1>Updated Source code for demo - should be the same on x86 and arm</h1> </body></html>");
+        return str;
     }
     
 }
